0647e592a9fbfba93ae4d5e293dab73931b3aa4a
Suresh Srinivas <suresh@apache.org>
2011-05-02 21:51:37 +0000

    Merging change r1078924 for HDFS-1665 from federation branch
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hdfs/branches/yahoo-merge@1098806 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hdfs/server/datanode/DataStorage.java
+++ src/java/org/apache/hadoop/hdfs/server/datanode/DataStorage.java
@@ -38,7 +38,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.FileUtil.HardLink;
 import org.apache.hadoop.fs.HardLink;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.hdfs.DFSConfigKeys;
