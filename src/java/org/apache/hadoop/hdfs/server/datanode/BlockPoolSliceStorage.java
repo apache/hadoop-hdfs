12ca83a3894cc765d29f6b02be07322f04922d94
Suresh Srinivas <suresh@apache.org>
2011-05-19 08:21:21 +0000

    Merging change r1104407 for HDFS-1881 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hdfs/branches/yahoo-merge@1124587 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hdfs/server/datanode/BlockPoolSliceStorage.java
+++ src/java/org/apache/hadoop/hdfs/server/datanode/BlockPoolSliceStorage.java
@@ -465,8 +465,12 @@ public class BlockPoolSliceStorage extends Storage {
     // do the link
     int diskLayoutVersion = this.getLayoutVersion();
     // hardlink finalized blocks in tmpDir
-    DataStorage.linkBlocks(fromDir, new File(toDir,
-        DataStorage.STORAGE_DIR_FINALIZED), diskLayoutVersion, new HardLink());
+    HardLink hardLink = new HardLink();
+    DataStorage.linkBlocks(new File(fromDir, DataStorage.STORAGE_DIR_FINALIZED), 
+      new File(toDir,DataStorage.STORAGE_DIR_FINALIZED), diskLayoutVersion, hardLink);
+    DataStorage.linkBlocks(new File(fromDir, DataStorage.STORAGE_DIR_RBW), 
+        new File(toDir, DataStorage.STORAGE_DIR_RBW), diskLayoutVersion, hardLink);
+    LOG.info( hardLink.linkStats.report() );
   }
 
   protected void corruptPreUpgradeStorage(File rootDir) throws IOException {
