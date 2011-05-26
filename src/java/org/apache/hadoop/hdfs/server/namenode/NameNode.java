e387b515ae1b0c629340928b4d891a3b55ef8f47
Jitendra Nath Pandey <jitendra@apache.org>
2011-05-26 18:26:10 +0000

    Merged r1064922 for HDFS-1335 from trunk.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hdfs/branches/yahoo-merge@1128019 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ src/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -82,6 +82,7 @@ import org.apache.hadoop.hdfs.server.protocol.UpgradeCommand;
 import org.apache.hadoop.http.HttpServer;
 import org.apache.hadoop.io.EnumSetWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.ipc.ProtocolSignature;
 import org.apache.hadoop.ipc.RPC;
 import org.apache.hadoop.ipc.Server;
 import org.apache.hadoop.metrics2.lib.DefaultMetricsSystem;
@@ -188,6 +189,14 @@ public class NameNode implements NamenodeProtocols, FSConstants {
     }
   }
     
+
+  @Override
+  public ProtocolSignature getProtocolSignature(String protocol,
+      long clientVersion, int clientMethodsHash) throws IOException {
+    return ProtocolSignature.getProtocolSigature(
+        this, protocol, clientVersion, clientMethodsHash);
+  }
+
   public static final int DEFAULT_PORT = 8020;
 
   public static final Log LOG = LogFactory.getLog(NameNode.class.getName());
